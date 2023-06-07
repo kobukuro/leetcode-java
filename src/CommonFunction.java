f18b5b76c1a3d5c8dbd479c45ec311e567f8846a
PeterChen <sanpeter992000@gmail.com>
2023-06-07 19:04:54 +0800

    226. Invert Binary Tree

---
+++ src/CommonFunction.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CommonFunction {
+    public List<Integer> levelOrderTraversal(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return res;
+    }
+}
