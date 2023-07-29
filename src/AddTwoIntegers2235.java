72d64547c1e83637987baa544483c53fe7ddef48
PeterChen <sanpeter992000@gmail.com>
2023-07-29 10:19:49 +0800

    2235. Add Two Integers

---
+++ src/AddTwoIntegers2235.java
@@ -0,0 +1,11 @@
+public class AddTwoIntegers2235 {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 5;
+        System.out.println(new AddTwoIntegers2235().sum(num1, num2)); // 17
+    }
+}
