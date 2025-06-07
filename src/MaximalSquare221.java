e3e247e8716512bf32f21f0f8f9dd9fda17c901a
Peter Chen <sanpeter992000@gmail.com>
2025-06-07 12:05:44 +0800

    Add an optimized solution for the Maximal Square problem using dynamic programming.

--- src/MaximalSquare221.java
+++ src/MaximalSquare221.java
@@ -15,6 +15,27 @@ public class MaximalSquare221 {
         return maxEdge * maxEdge;
     }
 
+    public int OptimizedMaximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] dp = new int[cols + 1];
+        int maxEdge = 0;
+        int prev = 0;
+        for (int r = 1; r < rows + 1; r++) {
+            for (int c = 1; c < cols + 1; c++) {
+                int temp = dp[c];
+                if (matrix[r - 1][c - 1] == '1') {
+                    dp[c] = Math.min(Math.min(dp[c], dp[c - 1]), prev) + 1;
+                    maxEdge = Math.max(maxEdge, dp[c]);
+                } else {
+                    dp[c] = 0;
+                }
+                prev = temp;
+            }
+        }
+        return maxEdge * maxEdge;
+    }
+
     public static void main(String[] args) {
         MaximalSquare221 solution = new MaximalSquare221();
         char[][] matrix = {
@@ -25,5 +46,6 @@ public class MaximalSquare221 {
                 {'1', '1', '1', '1', '1'}
         };
         System.out.println(solution.maximalSquare(matrix)); // Output: 4
+        System.out.println(solution.OptimizedMaximalSquare(matrix)); // Output: 4
     }
 }
