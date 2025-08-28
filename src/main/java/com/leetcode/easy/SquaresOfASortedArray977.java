260f9fc8abe841a9d2de37e2f459154e3cb9378d
Peter Chen <sanpeter992000@gmail.com>
2025-08-28 09:43:12 +0800

    Add unit tests for the "Squares of a Sorted Array" problem.

--- src/main/java/com/leetcode/easy/SquaresOfASortedArray977.java
+++ src/main/java/com/leetcode/easy/SquaresOfASortedArray977.java
@@ -1,11 +1,11 @@
-/*
-Time Complexity: O(N), where N is the length of the input array.
-Space Complexity: O(N) if you take output into account and O(1) otherwise.
-*/
-
-import java.util.Arrays;
+// Tags: Array, Two Pointers
+package com.leetcode.easy;
 
 public class SquaresOfASortedArray977 {
+    /*
+        Time complexity: O(N), where N is the length of the input array.
+        Space complexity: O(N) if you take output into account and O(1) otherwise.
+     */
     public int[] sortedSquares(int[] nums) {
         int[] res = new int[nums.length];
         int left = 0;
@@ -24,11 +24,5 @@ public class SquaresOfASortedArray977 {
             index--;
         }
         return res;
-    }
-
-    public static void main(String[] args) {
-        SquaresOfASortedArray977 solution = new SquaresOfASortedArray977();
-        assert Arrays.equals(solution.sortedSquares(new int[]{-4, -1, 0, 3, 10}), new int[]{0, 1, 9, 16, 100});
-        assert Arrays.equals(solution.sortedSquares(new int[]{-7, -3, 2, 3, 11}), new int[]{4, 9, 9, 49, 121});
     }
 }
