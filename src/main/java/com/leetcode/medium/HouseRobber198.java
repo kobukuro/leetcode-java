13eb3352b848bfd139ab29dcf46eb331ad0438c4
Peter Chen <sanpeter992000@gmail.com>
2025-09-06 09:33:36 +0800

    Add unit tests for the "House Robber" problem.

--- src/main/java/com/leetcode/medium/HouseRobber198.java
+++ src/main/java/com/leetcode/medium/HouseRobber198.java
@@ -1,11 +1,13 @@
-// Array, Dynamic Programming
+// Tags: Dynamic Programming
+package com.leetcode.medium;
+
 public class HouseRobber198 {
     /*
-        Time Complexity: O(N) since we have a loop from 0...nums.length-1,
+        Time complexity: O(N) since we have a loop from 0...nums.length-1,
         and we simply use the pre-calculated values of our dynamic programming table
         for calculating the current value in the table which is a constant time operation.
 
-        Space Complexity: O(N) which is used by the table.
+        Space complexity: O(N) which is used by the table.
      */
     public int robTabulation(int[] nums) {
         if (nums.length == 1) {
@@ -17,15 +19,9 @@ public class HouseRobber198 {
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < dp.length; i++) {
+        for (int i = 2; i < nums.length; i++) {
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
-        return dp[dp.length - 1];
-    }
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 1};
-        System.out.println(new HouseRobber198().robTabulation(nums));
-        // 4
+        return dp[nums.length - 1];
     }
 }
