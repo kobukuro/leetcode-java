8b80f627cda3ff48d13594cf31cc96b2e8641023
Peter Chen <sanpeter992000@gmail.com>
2025-09-08 10:52:50 +0800

    Optimize WordSearch79 visited tracking with boolean array.

--- src/main/java/com/leetcode/medium/WordSearch79.java
+++ src/main/java/com/leetcode/medium/WordSearch79.java
@@ -1,23 +1,19 @@
 // Tags: Backtracking, DFS
 package com.leetcode.medium;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class WordSearch79 {
-    private boolean dfs(int r, int c, char[][] board, String word, int index, Set<String> visited) {
-        String key = r + ";" + c;
+    private boolean dfs(int r, int c, char[][] board, String word, int index, boolean[][] visited) {
         if (index == word.length()) {
             return true;
         }
-        if (r < 0 || r == board.length || c < 0 || c == board[0].length || visited.contains(key) || word.charAt(index) != board[r][c]) {
+        if (r < 0 || r == board.length || c < 0 || c == board[0].length || visited[r][c] || word.charAt(index) != board[r][c]) {
             return false;
         }
-        visited.add(key);
+        visited[r][c] = true;
         if (this.dfs(r + 1, c, board, word, index + 1, visited) || this.dfs(r - 1, c, board, word, index + 1, visited) || this.dfs(r, c + 1, board, word, index + 1, visited) || this.dfs(r, c - 1, board, word, index + 1, visited)) {
             return true;
         }
-        visited.remove(key);
+        visited[r][c] = false;
         return false;
     }
     /*
@@ -26,7 +22,9 @@ public class WordSearch79 {
         Space complexity: O(w)
      */
     public boolean exist(char[][] board, String word) {
-        Set<String> visited = new HashSet<>();
+        int rows = board.length;
+        int cols = board[0].length;
+        boolean[][] visited = new boolean[rows][cols];
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
                 if (this.dfs(r, c, board, word, 0, visited)) {
